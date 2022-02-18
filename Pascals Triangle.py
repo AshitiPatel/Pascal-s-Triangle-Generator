08117c3c167349584f11d59fc584d9e41f8e95ab
Ashiti Patel <79819794+AshitiPatel@users.noreply.github.com>
2022-02-18 17:11:56 -0500

    Add files via upload

---
+++ Pascals Triangle.py
@@ -0,0 +1,30 @@
+# Function to print
+# first n lines of
+# Pascal's Triangle
+def printPascal(n) :
+	
+	# Go through every line
+	# and print entries in it
+	for line in range(0, n) :
+		
+		# Every line has number of
+		# integers equal to line
+		# number
+		for i in range(0, line + 1) :
+			print(binomialCoeff(line, i),
+				" ", end = "")
+		print()
+	
+# for details of this function
+def binomialCoeff(n, k) :
+	res = 1
+	if (k > n - k) :
+		k = n - k
+	for i in range(0 , k) :
+		res = res * (n - i)
+		res = res // (i + 1)
+	
+	return res
+
+n = int(input('Number of rows: '))
+printPascal(n)
